13b293fe1c29392e1e07d2817ae0a0986930794c
Gripokid <gripokid@gmail.com>
2019-05-18 15:03:56 +0300

    timer was added

--- js/script.js
+++ js/script.js
@@ -34,4 +34,50 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // Timer
+
+    let deadline = '2019-05-19';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours =  Math.floor((t/1000/3600));
+
+        return {
+            'total' : t, 
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer. querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+        
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            // for (var i in t) {
+            //     if (t[i] < 10) {
+            //         t[i] = '0' + t[i];
+            //     }
+            // }
+            hours.textContent = ('00' + t.hours).substr(('00' + t.hours).length - 2);
+            minutes.textContent = ('00' + t.minutes).substr(('00' + t.minutes).length - 2);
+            seconds.textContent = ('00' + t.seconds).substr(('00' + t.seconds).length - 2);
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
